675262b14706b8ec4f3eaa13eef7400738bdca3f
Javier Chacana <javier.chacana@codurance.com>
2020-04-09 12:50:38 +0200

    Added new function that will create a Post. For now, it will only transfer the data from the endpoint to a published event. Our listener understands it and logs it. Next step will be to read the data from event and append it to db record

--- eventBridgePublish/handler.js
+++ eventBridgePublish/handler.js
@@ -1,11 +1,14 @@
 // handler.js
 const aws = require('aws-sdk')
+const middy = require('middy')
+const { cors } = require('middy/middlewares')
 
-const handler = async (event, context, callback) => {
+const eventBridge = new aws.EventBridge({apiVersion: '2015-10-07'}); 
+
+var publish = async (event, context, callback) => {
   console.log(event)
   const userId = event.pathParameters.user_id;
   
-  const eventBridge = new aws.EventBridge({apiVersion: '2015-10-07'}); 
   const obj = {data: userId}
   await eventBridge.putEvents({
     Entries: [{
@@ -28,7 +31,39 @@ const handler = async (event, context, callback) => {
   }
   return callback(null, response)
 }
+publish = middy(publish).use(cors())
 
-const publish = handler
 
-module.exports = { publish }
+var createPost = async (event, context, callback) => {
+  console.log(event);
+  var body;
+  try {
+    body = JSON.parse(event.body)
+  } catch (e) {
+    body = event.body
+  }
+  console.log(body)
+  await eventBridge.putEvents({
+    Entries: [{
+      Source: 'codurance.post',
+      DetailType: 'CodurancePost',
+      Detail: JSON.stringify(body)
+    }]
+  }, (err, data) => {
+    if(err) console.log("ERROR", err.stack)
+    else console.log(data)
+  })
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {"success": "event published"},
+      null,
+      2
+    )
+  }
+  return callback(null, response)
+}
+createPost = middy(createPost).use(cors())
+
+module.exports = { publish, createPost }
